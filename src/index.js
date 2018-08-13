9e1eef868888878b354b5d96d3647c7ecb7f9b1a
Cornelius Ehmke <cornelius.ehmke@gmail.com>
2018-08-13 19:47:54 +1200

    Move app code into app folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import registerServiceWorker from 'services/registerServiceWorker';
+
+import App from 'App/App';
+
+ReactDOM.render(
+    <App />
+, document.getElementById('root'));
+
 registerServiceWorker();
